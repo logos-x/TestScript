92c6b639616a9eca64a8ac6eda6a12035178d991
Tran Quang Tai <trantai13502@gmail.com>
2025-04-09 11:01:22 +0700

    Test script for test case TC_03

---
+++ TC03.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Khởi tạo trình duyệt
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+try:
+    driver.get("http://localhost:5173/register")
+
+    wait = WebDriverWait(driver, 10)
+
+    # Nhập username, email và password
+    email_input = wait.until(EC.presence_of_element_located((By.NAME, "username")))
+    email_input.send_keys("user3")
+
+    email_input = wait.until(EC.presence_of_element_located((By.NAME, "email")))
+    email_input.send_keys("user3@gmail.com")
+
+    password_input = driver.find_element(By.NAME, "password")
+    password_input.send_keys("User3@12")
+
+    # Click nút Create Account
+    sign_in_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Create Account')]")
+    sign_in_button.click()
+
+    # Đợi và xử lý alert nếu xuất hiện
+    try:
+        alert = WebDriverWait(driver, 5).until(EC.alert_is_present())
+        alert_text = alert.text
+        print(f"Thông báo từ hệ thống: {alert_text}")
+        alert.accept()
+
+        if alert_text.strip() == "Vui lòng nhập mật khâu đủ 8 ký tự để đảm bảo yêu cầu bảo mật":
+            print("PASSED")
+        else:
+            print(f"FAILED - Nội dung alert không đúng mong đợi")
+
+    except:
+        print("FAILED - Không có alert xuất hiện")
+
+
+    input("Nhấn Enter để tiếp tục...")
+finally:
+    driver.quit()
